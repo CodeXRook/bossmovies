6eabe92513144359db9168450f75660f7b4deab8
CodeXRook <lacekitfly@gmail.com>
2019-04-14 14:27:00 -0400

    working on ratings in services, this message will update once completed

--- services/ratings.js
+++ services/ratings.js
@@ -23,3 +23,9 @@ RatingService.create = ( stars, movie_id) => {
     `;
     return db.one(sql, {stars, movie_id});
 };
+
+//UPDATED
+RatingService.update = ( id, stars, movie_id) => {
+    const sql = `
+    UPDATE
+    ratings

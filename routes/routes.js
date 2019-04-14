35f7f9ef5322b7e9c2190f049cb88773c5fe6725
CodeXRook <lacekitfly@gmail.com>
2019-04-14 15:16:56 -0400

    working on ratings in routes, this msg will update once completed

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,6 @@
 const express = require('express');
 const ratingRouter = express.Router();
 const RatingService = require('../services/ratings');
+
+// Get rating info 
+ratingRouter.get('/:id', (req, res, next)=>{

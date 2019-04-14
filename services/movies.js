94743adcbb555d0c41b99e27845c8f779d442309
CodeXRook <lacekitfly@gmail.com>
2019-04-14 14:18:23 -0400

    serivces for movies, this msg will update when completed

--- services/movies.js
+++ services/movies.js
@@ -2,4 +2,9 @@ const { db } =require('./dbConnect');
 const MovieService = {};
 
 //CREATE
-MovieService.create = (title, genre_id, img_url) =>
+MovieService.create = (title, genre_id, img_url) => {
+const sql = `
+INSERT INTO 
+movies ( title, genre_id, img_url)
+
+};

635fe7473d0b865166e59d867d4d65a5558cc94e
CodeXRook <lacekitfly@gmail.com>
2019-04-14 13:15:55 -0400

    index done

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/', router);
+app.use('/'I, router);
 
 app.use((err, req, res, next)=>{
     res.status(400).json({ error: err.toString() });
  });
 
+
  app.listen(port, () => {
     console.log(`App is running on port: ${port}`);
+
 });
+
+//index done

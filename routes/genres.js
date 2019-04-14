e7d6625b9f21584acc3c81f17df03402a79182c0
CodeXRook <lacekitfly@gmail.com>
2019-04-14 15:03:47 -0400

    working on genres in routes this messege will update once completed

--- routes/genres.js
+++ routes/genres.js
@@ -5,3 +5,7 @@ const GenreService = require('../services/genres');
 // GET GENRE
 genreRouter.get('/', (req, res, next) => {
     const { id } = req.params;
+
+    GenreService.read(id)
+    .then(data => {
+        res.json(data);

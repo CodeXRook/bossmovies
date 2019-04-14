d4e4bbe44662d78efae14af044a65d7a7bd7e583
CodeXRook <lacekitfly@gmail.com>
2019-04-14 14:34:41 -0400

    inputing database, this message will update once complete

--- seed.sql
+++ seed.sql
@@ -4,13 +4,19 @@ CREATE DATABASE bossmovie_app;
 \c bossmovie_app;
 
  CREATE TABLE movies (
-id SERIAL PRIMARY KEY NOT NULL,
-title VARCHAR NOT NULL,
-genre_id INT UNIQUE NOT NULL,
-img_url VARCHAR NOT NULL
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR NOT NULL,
+  genre_id INT UNIQUE NOT NULL,
+  img_url VARCHAR NOT NULL
 );
 
-CREATE TABLE genres(
- id INT REFERENCES movies(genre_id),
-name VARCHAR NOT NULL
+ CREATE TABLE genres(
+  id INT REFERENCES movies(genre_id),
+  name VARCHAR NOT NULL
 );
+
+CREATE TABLE ratings(
+
+);
+
+

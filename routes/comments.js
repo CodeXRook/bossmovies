5910a8100d9a01b50688b15eda4dfc006f383247
CodeXRook <lacekitfly@gmail.com>
2019-04-14 14:55:33 -0400

    working on comments in routes, this messege will update once completed

--- routes/comments.js
+++ routes/comments.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const commentRouter = express.Router();
 const CommentService = require('../services/comments');
+
+//GET COMMENT INFO
+commentRouter.get('/:id', (req, res, next)=> {
+    
+}
